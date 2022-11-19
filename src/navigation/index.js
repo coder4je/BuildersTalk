61ef27d9fd9fe7f85b2f65b723d8bee337e0ac04
Taemin Kim <taeminkimny@gmail.com>
2022-11-19 01:04:07 -0500

    Initiated the backend server with aws amplify

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,9 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import ChatsScreen from "../screens/ChatsScreen";
 import ChatScreen from "../screens/ChatScreen";
 import MainTabNavigator from "./MainTabNavigator";
+import ContactsScreen from "../screens/ContactsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -18,6 +18,7 @@ const Navigator = () => {
           options={{ headerShown: false }}
         />
         <Stack.Screen name="Chat" component={ChatScreen} />
+        <Stack.Screen name="Contacts" component={ContactsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
