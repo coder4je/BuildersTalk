7342cc08973a36883723ccd12f2da10c3e134456
Taemin Kim <taeminkimny@gmail.com>
2022-12-09 15:44:46 -0500

    Reset Backend

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Navigator from "./src/navigation/index";
-import { Amplify } from "aws-amplify";
-import { withAuthenticator } from "aws-amplify-react-native";
-import awsconfig from "./src/aws-exports";
+// import { Amplify } from "aws-amplify";
+// import { withAuthenticator } from "aws-amplify-react-native";
+// import awsconfig from "./src/aws-exports";
 
-Amplify.configure(awsconfig);
+// Amplify.configure(awsconfig);
 
 const App = () => {
   return (
@@ -25,4 +25,5 @@ const styles = StyleSheet.create({
   },
 });
 
-export default withAuthenticator(App);
+// export default withAuthenticator(App);
+export default App;
--- src/Screens/ChatScreen.js
+++ src/Screens/ChatScreen.js
@@ -1,7 +1,6 @@
 import { useEffect } from "react";
 import {
   View,
-  Text,
   ImageBackground,
   StyleSheet,
   FlatList,
